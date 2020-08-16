795d45d96331b4070ff5e6b743bd5c6c301dce3e
ngocptt <ngocphamttdt@gmail.com>
2020-08-16 17:11:26 +0700

    Add die method for Mouse

--- Mouse.js
+++ Mouse.js
@@ -1,8 +1,8 @@
 function Mouse (name) {
     this.name = name;
-    this.dead = false;
+    this.isDead = false;
 }
-Mouse.prototype.die = function() {
-    this.dead = true;
+Mouse.prototype.die = function (){
+    this.isDead = true;
 }
 module.exports = Mouse;
